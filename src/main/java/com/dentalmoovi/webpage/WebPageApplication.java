47d0370909bcb2301e94c585835884015f888686
Oscar Gómez <98911168+Oagomezr@users.noreply.github.com>
2023-06-06 07:38:13 -0500

    fisrt commit

---
+++ src/main/java/com/dentalmoovi/webpage/WebPageApplication.java
@@ -0,0 +1,14 @@
+package com.dentalmoovi.webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
+public class WebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPageApplication.class, args);
+	}
+
+}
