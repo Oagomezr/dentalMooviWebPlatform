ababfa1397541006b645b262eaf0939fda563118
Oscar Gómez <98911168+Oagomezr@users.noreply.github.com>
2023-06-07 16:26:13 -0500

    feat: Presentation Layer part 1

---
+++ src/main/java/com/dentalmoovi/webpage/rest/UserController.java
@@ -0,0 +1,85 @@
+package com.dentalmoovi.webpage.rest;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.dentalmoovi.webpage.dtos.UserDTO;
+import com.dentalmoovi.webpage.exceptions.DataExistException;
+import com.dentalmoovi.webpage.exceptions.DataNotFoundException;
+import com.dentalmoovi.webpage.services.UserSer;
+
+@RestController
+@RequestMapping("/public")
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+    
+    @Autowired
+    private UserSer userSer;
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers(){
+        List <UserDTO> users = userSer.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO){
+        try {
+            UserDTO userCreated = userSer.createUser(userDTO);
+            return ResponseEntity.created(URI.create("/users/"+userCreated.getIdUser())).body(userCreated);
+        } catch (DataExistException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id){
+        try {
+            UserDTO userGetted = userSer.getUser(id);
+            return ResponseEntity.ok(userGetted);
+        } catch (DataNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> actualizarUsuario(@PathVariable("id") Long id, @RequestBody UserDTO usersDTO) {
+        try {
+            UserDTO userUpdated = userSer.updateUser(id, usersDTO);
+            return ResponseEntity.ok(userUpdated);
+        } catch (DataNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        try {
+            userSer.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } catch (DataNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/checkValueExists/{field}/{value}")
+    public boolean checkValueExists(@PathVariable String field, @PathVariable String value) {
+        return userSer.checkValueExists(field, value);
+    }
+}
